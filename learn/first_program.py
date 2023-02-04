59cf3212f177fbfb7334aa651331f6f7186f67b4
saravanankrishnarajan <32175950+saravanankrishnarajan@users.noreply.github.com>
2023-02-04 10:19:22 +0100

    first program

--- learn/first_program.py
+++ learn/first_program.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Example").getOrCreate()
 
-df = spark .read.csv("departments.csv",header=True,inferSchema=True)
+df = spark.read.csv("data/bank-data.csv",header=True,inferSchema=True)
 
 df.printSchema()
 
@@ -10,4 +10,14 @@ df.show(10)
 
 print("Number of rows:", df.count())
 
-selecteddf.select("department")
+selected_df = df.select("marital","age")
+
+aggregated_df_count = selected_df.groupby("marital").agg({"age":"count"})
+
+aggregated_df_count.show()
+
+aggregated_df_age_mean = selected_df.groupby("marital").agg({"age":"mean"})
+
+aggregated_df_age_mean.show()
+
+spark.stop()
